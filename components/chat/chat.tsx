950ee37f48333241bf664db3eb699eed0d77c1c6
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-08-10 18:04:17 +0100

    feat: removed unused state, and added ai response to message state

--- components/chat/chat.tsx
+++ components/chat/chat.tsx
@@ -1,6 +1,8 @@
-import ChatTopbar from "./chat-topbar";
+import { useState } from "react";
 import { ChatList } from "./chat-list";
-import React from "react";
+import { customerSupportPrompt } from "@/lib/langchain";
+import { product_details } from "@/types/product-details";
+import ChatTopbar from "./chat-topbar";
 
 interface ChatProps {
   messages?: any[];
@@ -8,22 +10,43 @@ interface ChatProps {
 }
 
 export function Chat({ isMobile }: ChatProps) {
-  const [messagesState, setMessages] = React.useState<any[]>([]);
+  const [messagesState, setMessages] = useState<any[]>([]);
+  const [loadingResponse, setLoadingResponse] = useState(false);
 
-  const sendMessage = (newMessage: any) => {
-    setMessages([...messagesState, newMessage]);
+  const sendMessage = async (newMessage: any) => {
+    setMessages((prevMessage) => [...prevMessage, newMessage]);
+
+    try {
+      setLoadingResponse(true);
+      const response = await customerSupportPrompt(
+        newMessage.message,
+        "TechNest Solutions",
+        JSON.stringify(product_details)
+      );
+
+      const aiMessage = {
+        id: messagesState.length + 1,
+        name: "AI Customer Support",
+        avatar: "",
+        message: response,
+      };
+      setMessages((prevMessages) => [...prevMessages, aiMessage]);
+      setLoadingResponse(false);
+    } catch (error) {
+      console.log("failed to generate", error);
+    }
   };
 
   console.log("message array:", messagesState);
 
   return (
-    <div className='flex flex-col justify-between w-full h-full'>
+    <div className='flex flex-col justify-between w-[50vw] h-[90vh] border'>
       <ChatTopbar />
-
       <ChatList
         messages={messagesState}
         sendMessage={sendMessage}
         isMobile={isMobile}
+        loading={loadingResponse}
       />
     </div>
   );
