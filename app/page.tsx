90a619334fce47996b0e9f89bf68bdf242180360
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-08-10 18:01:49 +0100

    feat: removed unused sit header

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 
   return (
     <main className='flex h-[calc(100dvh)] flex-col items-center justify-center p-4 md:px-24 py-32 gap-4'>
-      <div className='z-10 border rounded-lg max-w-5xl w-full h-full text-sm lg:flex'>
+      <div className='z-10 flex flex-col items-center justify-center rounded-lg max-w-5xl w-full h-full text-sm lg:flex'>
         <ChatLayout defaultLayout={defaultLayout} navCollapsedSize={8} />
       </div>
     </main>
