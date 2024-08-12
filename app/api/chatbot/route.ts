b8fc9d0381789b8a73d7c04ea9c5076583fd57e4
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-08-12 15:25:28 +0100

    feat: route endpoint for chatbot

--- app/api/chatbot/route.ts
+++ app/api/chatbot/route.ts
@@ -1,18 +1,18 @@
 import { customerSupportPrompt } from "@/lib/langchain";
-import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request, res: NextApiResponse) {
+export async function POST(req: Request) {
   try {
     const { question } = await req.json();
-
-    
+    console.log("questions coming from the frontend", question)
     const response = await customerSupportPrompt(question);
-    return NextResponse.json(response);
+
+    return NextResponse.json(response); 
   } catch (error: any) {
     console.error("Error in chatbot API:", error);
-    res
-      .status(500)
-      .json({ error: "Internal Server Error", message: error.message });
+    return NextResponse.json(
+      { error: "Internal Server Error", message: error.message },
+      { status: 500 }
+    );
   }
 }
